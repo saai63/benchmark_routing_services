113b26b24ae0ebbe8b893d8af22c54b5c253dc2d
saai63 <saai63@users.noreply.github.com>
2024-07-08 23:45:54 +0200

    Create benchmark_services.sh

---
+++ benchmark_services.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Constants
+VENV_NAME="benchmark_services"
+VENV_PATH="$HOME/python_envs"
+VENV="$VENV_PATH"/"$VENV_NAME"
+
+prepare_python_env() {
+    if [ ! -d "$VENV_PATH" ]; then
+        echo "Creating folder $VENV_PATH"
+        mkdir "$VENV_PATH"
+    fi
+    python3 -m venv "$VENV"
+    source $VENV/bin/activate
+    pip install -r requirements.txt
+}
+
+prepare_python_env
+deactivate
